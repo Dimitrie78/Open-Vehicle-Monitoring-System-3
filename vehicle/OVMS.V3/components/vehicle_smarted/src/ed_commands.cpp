66918157ad3dbe9d83fbec57a86f0d0c4c2327eb
Dimitrie78 <egon@heuer-humfeld.de>
2019-10-21 17:14:36 +0200

    Revert "Update ed_commands.cpp"
    This reverts commit 58d97aa69e38826b9f597fac8cc9fa523e5584c9.

--- vehicle/OVMS.V3/components/vehicle_smarted/src/ed_commands.cpp
+++ vehicle/OVMS.V3/components/vehicle_smarted/src/ed_commands.cpp
@@ -126,8 +126,6 @@ bool OvmsVehicleSmartED::CommandSetRecu(bool on) {
   frame.MsgID = 0x236;
   frame.data.u8[0] = (on == true ? 0x02 : 0x04);
   frame.Write();
-  vTaskDelay(50 / portTICK_PERIOD_MS);
-  frame.Write();
 
   return true;
 }
