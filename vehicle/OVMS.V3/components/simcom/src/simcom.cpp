ef094d6cb4ddb8640b1395894184e630c8e6818a
Dimitrie78 <egon@heuer-humfeld.de>
2020-04-29 18:39:16 +0200

    Update simcom.cpp

--- vehicle/OVMS.V3/components/simcom/src/simcom.cpp
+++ vehicle/OVMS.V3/components/simcom/src/simcom.cpp
@@ -1009,7 +1009,7 @@ void simcom::StandardLineHandler(int channel, OvmsBuffer* buf, std::string line)
 void simcom::PowerCycle()
   {
   ESP_LOGI(TAG, "Power Cycle");
-  MyNotify.NotifyString("info", "modem.powercycle", "Modem Power Cycle");
+  // MyNotify.NotifyString("info", "modem.powercycle", "Modem Power Cycle");
   uart_flush(m_uartnum);
 #ifdef CONFIG_OVMS_COMP_MAX7317
   MyPeripherals->m_max7317->Output(MODEM_EGPIO_PWR, 0); // Modem EN/PWR line low
