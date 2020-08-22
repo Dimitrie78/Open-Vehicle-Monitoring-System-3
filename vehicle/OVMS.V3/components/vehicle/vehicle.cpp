75024603dc2adc15f4eb6124182ed6fde7c87495
Dimitrie78 <egon@heuer-humfeld.de>
2020-08-22 13:14:59 +0200

    OvmsVehicle::PollerReceive
    VEHICLE_POLL_TYPE_OBDIIEXTENDED Bug fix.
    
    correct first frame description, m_poll_ml_remain length and IncomingPollReplyInternal to the correct values.
    
    add missing m_poll_ml_remain, m_poll_ml_frame to single frame and
     correct length to IncomingPollReplyInternal

--- vehicle/OVMS.V3/components/vehicle/vehicle.cpp
+++ vehicle/OVMS.V3/components/vehicle/vehicle.cpp
@@ -2244,8 +2244,8 @@ void OvmsVehicle::PollerReceive(CAN_frame_t* frame)
           (frame->data.u8[2] == 0x40+m_poll_type)&&
           ((frame->data.u8[4]+(((uint16_t) frame->data.u8[3]) << 8))  == m_poll_pid))
         {
-        // First frame is 4 bytes header (2 ISO-TP, 2 OBDII), 4 bytes data:
-        // [first=1,lenH] [lenL] [type+40] [pid] [data0] [data1] [data2] [data3]
+        // First frame is 5 bytes header (2 ISO-TP, 3 OBDII), 3 bytes data:
+        // [first=1,lenH] [lenL] [type+40] [pid] [pid] [data0] [data1] [data2]
         // Note that the value of 'len' includes the OBDII type and pid bytes,
         // but we don't count these in the data we pass to IncomingPollReply.
         //
@@ -2274,12 +2274,12 @@ void OvmsVehicle::PollerReceive(CAN_frame_t* frame)
         txframe.Write();
 
         // prepare frame processing, first frame contains first 4 bytes:
-        m_poll_ml_remain = (((uint16_t)(frame->data.u8[0]&0x0f))<<8) + frame->data.u8[1] - 3;
+        m_poll_ml_remain = (((uint16_t)(frame->data.u8[0]&0x0f))<<8) + frame->data.u8[1] - 3 - 3;
         m_poll_ml_offset = 3;
         m_poll_ml_frame = 0;
 
         //ESP_LOGD(TAG, "Poll ML first frame (frame=%d, remain=%d)",m_poll_ml_frame,m_poll_ml_remain);
-        IncomingPollReplyInternal(frame->origin, m_poll_type, m_poll_pid, &frame->data.u8[4], 4, m_poll_ml_remain);
+        IncomingPollReplyInternal(frame->origin, m_poll_type, m_poll_pid, &frame->data.u8[5], 3, m_poll_ml_remain);
         return;
         }
       else if (((frame->data.u8[0]>>4)==0x2)&&(m_poll_ml_remain>0))
@@ -2306,8 +2306,12 @@ void OvmsVehicle::PollerReceive(CAN_frame_t* frame)
       else if ((frame->data.u8[1] == 0x62)&&
                ((frame->data.u8[3]+(((uint16_t) frame->data.u8[2]) << 8)) == m_poll_pid))
         {
+        uint16_t len;
+        len = (uint16_t) frame->data.u8[0] - 3;
         m_poll_ml_remain = 0;
-        IncomingPollReplyInternal(frame->origin, m_poll_type, m_poll_pid, &frame->data.u8[4], 4, 0);
+        m_poll_ml_frame = 0;
+        //ESP_LOGD(TAG, "Poll ML single frame (frame=%d, remain=%d)",m_poll_ml_frame,m_poll_ml_remain);
+        IncomingPollReplyInternal(frame->origin, m_poll_type, m_poll_pid, &frame->data.u8[4], len, 0);
         }
       break;
     }
