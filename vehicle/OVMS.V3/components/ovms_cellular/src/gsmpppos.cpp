77f37e6a11f8fd9b63eea20e93d13036e42fad3b
ZorG <100448801+zorgms@users.noreply.github.com>
2024-12-18 15:46:19 +0100

    Update gsmpppos.cpp

--- vehicle/OVMS.V3/components/ovms_cellular/src/gsmpppos.cpp
+++ vehicle/OVMS.V3/components/ovms_cellular/src/gsmpppos.cpp
@@ -160,11 +160,11 @@ static void GsmPPPOS_StatusCallback(ppp_pcb *pcb, int err_code, void *ctx)
     MyEvents.SignalEvent("system.modem.down",NULL);
     }
 
-  // Try to reconnect in 30 seconds. This is assuming the modem level
+  // Try to reconnect in 60 seconds. This is assuming the modem level
   // data channel is still open.
-  ESP_LOGI(TAG, "Attempting PPP reconnecting in 30 seconds...");
+  ESP_LOGI(TAG, "Attempting PPP reconnecting in 60 seconds...");
   // Note: We are in tiT task context here so use ppp_connect not pppapi_connect.
-  ppp_connect(pcb, 30);
+  ppp_connect(pcb, 60);
   }
 
 GsmPPPOS::GsmPPPOS(GsmMux* mux, int channel)
